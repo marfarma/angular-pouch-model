c73e498b8d346c54e90d85f9453892531abb27dc
Pauli Price <pauli.price@gmail.com>
2014-01-01 13:16:56 -0500

    htmlmode

--- docs/assets/js/app.js
+++ docs/assets/js/app.js
@@ -2,3 +2,10 @@
 
 // Declare app level module which depends on filters, and services
 var myApp = angular.module('myApp', ['myApp.directives', 'ui.bootstrap']);
+myApp.config(['$locationProvider', function AppConfig($locationProvider) {
+
+    // enable html5Mode for pushstate ('#'-less URLs)
+    $locationProvider.html5Mode(true);
+    $locationProvider.hashPrefix('!');
+
+}]);
