e9842a81bb49354c49fc63af6db50f4220831854
Pauli Price <pauli.price@gmail.com>
2014-01-01 04:03:32 -0500

    removed unused services

--- docs/assets/js/app.js
+++ docs/assets/js/app.js
@@ -1,15 +1,10 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-var myApp = angular.module('myApp', ['myApp.filters', 'myApp.services', 'elasticjs.service', 'myApp.directives', 'ui.bootstrap', 'dangle']).
+var myApp = angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'ui.bootstrap']).
         config(['$routeProvider', function ($routeProvider) {
-            $routeProvider.when('/dashboard', {templateUrl: 'partials/dashboard.html', controller: DashboardCtrl});
-            $routeProvider.when('/node/:nodeId', {templateUrl: 'partials/node.html', controller: NodeInfoCtrl});
-            $routeProvider.when('/search', {templateUrl: 'partials/search.html', controller: SearchCtrl});
-            $routeProvider.when('/query', {templateUrl: 'partials/query.html', controller: QueryCtrl});
-            $routeProvider.when('/graph', {templateUrl: 'partials/graph.html', controller: GraphCtrl});
             $routeProvider.when('/about', {templateUrl: 'partials/about.html'});
-            $routeProvider.otherwise({redirectTo: '/dashboard'});
+            $routeProvider.otherwise({redirectTo: '/'});
         }]);
 
 myApp.value('localStorage', window.localStorage);
