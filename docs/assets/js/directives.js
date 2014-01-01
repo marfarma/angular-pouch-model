c9768286e57f938454adc980923f8e6537ead343
Pauli Price <pauli.price@gmail.com>
2014-01-01 12:06:30 -0500

    fix template link

--- docs/assets/js/directives.js
+++ docs/assets/js/directives.js
@@ -15,7 +15,7 @@ angular.module('myApp.directives', []).
                 transclude: true,
                 scope: {heading: '@'},
                 controller: 'NavbarCtrl',
-                templateUrl: 'assets/template/navbar/navbar.html',
+                templateUrl: 'http://raw.github.com/marfarma/angular-pouch-model/master/docs/assets/template/navbar/navbar.html',
                 replace: true,
                 link: function ($scope, $element, $attrs, navbarCtrl) {
                     $scope.$location = $location;
