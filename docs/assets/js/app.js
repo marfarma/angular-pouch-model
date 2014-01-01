3c1a4244b7bcb5854fd2a51cb10e4651d3cf406f
Pauli Price <pauli.price@gmail.com>
2014-01-01 04:19:55 -0500

    strip more out

--- docs/assets/js/app.js
+++ docs/assets/js/app.js
@@ -1,10 +1,4 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-var myApp = angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'ui.bootstrap']).
-        config(['$routeProvider', function ($routeProvider) {
-            $routeProvider.when('/about', {templateUrl: 'partials/about.html'});
-            $routeProvider.otherwise({redirectTo: '/'});
-        }]);
-
-myApp.value('localStorage', window.localStorage);
+var myApp = angular.module('myApp', ['myApp.directives', 'ui.bootstrap']);
--- docs/assets/js/services.js
+++ docs/assets/js/services.js
@@ -1,9 +1,6 @@
 'use strict';
 
 /* Services */
-var serviceModule = angular.module('myApp.services', []);
-serviceModule.value('version', '0.4');
-
 
 
 
